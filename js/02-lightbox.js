068baf1561a57227368f5c71ed0336c147447216
vetalfor <vetal.formuha@gmail.com>
2023-08-23 14:28:10 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const renderImage = (arr, container) => {
+    const marcup = arr.map((item) => `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+    class="gallery__image"
+    src="${item.preview}"
+    alt="${item.description}" />
+    </a>
+    </li>`).join('');
+
+    container.insertAdjacentHTML('afterbegin', marcup);
+}
+renderImage(galleryItems, galleryList);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+    
+});
